a1be88ae2f39617a11410fc5a1bbde0afa0d27d9
Daniel Sottile <danielsottile@gmail.com>
2020-05-28 19:01:47 -0400

    changed the landing page slightly

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -32,31 +32,40 @@ export class LandingPage extends Component {
               <h3>Store your teams like never before!</h3>
             </header>
             <img className="landingimg" src="https://imgur.com/9wYBaMQ.png" alt="organize page"/>
-            <p>PokeTeams goes above and beyond the average pastebin in order for you to save
+            <p>PokeTeams goes above and beyond the average pastebin in order for you to import, save, 
             and organize your teams from Pokemon Showdown into a secure database.  Never lose a team again!
             Say goodbye to Pastebin's forever!
             </p>
           </section>
           <section>
             <header>
-              <h3>Organize, Edit, and Share Your Teams!</h3>
+              <h3>Upload, Edit, and Maintain your teams!</h3>
             </header>
             <img className="landingimg" src="https://imgur.com/HR1yjKA.png" alt="edit set"/>
-            <p>Once you've uploaded your teams, organize, edit, and share them to
-            your hearts content!  
+            <p>Once you've uploaded your teams, you'll be able to edit them by hand or by importing
+            individual sets!  The possibilities are endless!
             </p>
           </section>
           <section className="right">
             <header>
-                <h3>Search By Numerous Parameters and Export!</h3>
+                <h3>Search By Numerous Parameters!</h3>
             </header>
             <img className="landingimg" src="https://imgur.com/JwMxPdp.png" alt="search"/>
-            <p>PokeTeams lets you search the database by specific Pokemon species, alphabetical order,
-              popularity, and newly created, Favorite ones you like, and export ones you
-              want to use for your own Pokemon Showdown teams!
+            <p>PokeTeams lets you search the database by specific Pokemon species, alphabetical order
+               and date created.  In the future, you'll be able to search for specific teams
+              and by popularity!
             </p>
-            <img className="landingimg" src="https://imgur.com/JWIgCyL.png" alt="export"/>
           </section>
+          <section>
+            <header>
+              <h3>Share Your Teams and Pokemon!</h3>
+            </header>
+            <img className="landingimg" src="https://imgur.com/JWIgCyL.png" alt="export"/>
+            <p>Most importantly, you can share both full teams and Pokemon sets with anyone! Create awesome
+            teams and Pokemon and let the world know about your creations!
+            </p>
+          </section>
+          
        
       <Footer/>
       </Fragment>
