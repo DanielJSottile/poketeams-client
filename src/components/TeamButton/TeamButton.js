6204230d28f4f41f3667f95e998f2cd74f0298e0
Daniel Sottile <danielsottile@gmail.com>
2020-05-28 17:39:59 -0400

    fixed an issue with links on firefox

--- src/components/TeamButton/TeamButton.js
+++ src/components/TeamButton/TeamButton.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
 import UserContext from '../../contexts/UserContext';
 
 export default class TeamButton extends Component {
