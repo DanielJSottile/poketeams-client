c82784e6b9958f1132c64c18a46cda4d5153e701
Daniel Sottile <danielsottile@gmail.com>
2020-07-25 14:03:16 -0400

    Share Team Page

--- src/routes/ShareTeamPage/ShareTeamPage.js
+++ src/routes/ShareTeamPage/ShareTeamPage.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import GeneralContext from '../../contexts/GeneralContext';
 import apiService from '../../services/apiService';
 import TeamPublic from '../../components/Team-Public/Team-Public';
 
-export default class ShareTeamPage extends Component {
+const ShareTeamPage = (props) => {
 
-  static contextType = GeneralContext;
+  const GenCon = useContext(GeneralContext);
 
-  state = {
-    team: []
-  }
+  const [state, setState] = useState({team: []});
 
-  componentDidMount() {
+  const {addPublicSets} = GenCon;
 
-    const {addPublicSets} = this.context;
+  /* This acts as our ComponentDidMount that gets the team
+  specified in the url parameter.*/
 
-
-    apiService.getSingleTeam(this.props.match.params.team_id) // Get the single public team!
+  useEffect(() => {
+    apiService.getSingleTeam(props.match.params.team_id)
       .then(data => {
-        this.setState({team: [...this.state.team, data]})
-        apiService.getSetsForOneTeam(this.props.match.params.team_id)
-          .then(data => {
-            addPublicSets(data)
-          })
+        setState(oldVals => ({...oldVals, team: [data]}))
     })
-  }
 
+    /* We'll need to get the sets now because they may or may not be 
+    in the public teams at the moment (statistically almost 0 chance). 
+    The dependencies make sure it only updates 1 time.  */
 
-  render() {
-
-    
+      .then(() => {
+     apiService.getSetsForOneTeam(props.match.params.team_id)
+      .then(data => {
+        addPublicSets(data)
+      })
+    })
+  }, [props.match.params.team_id, addPublicSets]); 
 
     return (
       
       <div>
-       { this.state.team[0] ? <TeamPublic team={this.state.team[0]}/> : <h3>This team seems to not exist anymore</h3>}
+       { state.team[0] ? <TeamPublic team={state.team[0]}/> : <h3>This team seems to not exist anymore</h3>}
       </div>
     );
-  };
 };
+
+export default ShareTeamPage;
