1d72b474d3e57fb014f06ed86d870ab75050db8f
Daniel Sottile <danielsottile@gmail.com>
2020-07-25 12:41:20 -0400

    TeamListEdit

--- src/components/TeamList-Edit/TeamList-Edit.js
+++ src/components/TeamList-Edit/TeamList-Edit.js
@@ -1,28 +1,26 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import GeneralContext from '../../contexts/GeneralContext';
 import TeamEdit from '../Team-Edit/Team-Edit';
 
-export default class TeamList extends Component {
+const TeamListEdit = (props) => {
 
-  static contextType = GeneralContext;
+  const GenCon = useContext(GeneralContext);
 
+  const {userTeams, currentClickedFolder} = GenCon;
 
-  render() {
-
-    const {userTeams, currentClickedFolder,} = this.context;
-
-    const TeamList = userTeams
+  const TeamList = userTeams
     .filter(team => team.folder_id === currentClickedFolder.id)
     .map((team, i) => {
       return <TeamEdit className="btn" id={`${team.team_name}`} key={i} team={team}/>
     });
 
-    return (
-      <Fragment>
-        {TeamList.length > 0 ? TeamList : <h3>There Are No Teams!  Make Teams with Pokemon!</h3>}
-      </Fragment>
-    );
-  };
+  return (
+    <Fragment>
+      {TeamList.length > 0 ? TeamList : <h3>There Are No Teams!  Make Teams with Pokemon!</h3>}
+    </Fragment>
+  );
 };
+
+export default TeamListEdit;
 
 
