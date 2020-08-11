d92ecb400554810b9ae20e2418a7981d21d2b4f3
Daniel Sottile <danielsottile@gmail.com>
2020-08-10 21:42:47 -0400

    fixed utils to be typescript

---
+++ src/components/Utils/PublicOnlyRoute.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import TokenService from '../../services/token-service';
+
+export interface IProps {
+  component: any;
+  path: string;
+}
+
+export default function PublicOnlyRoute({ component, ...props }: IProps) {
+  const Component = component
+  return (
+    <Route
+      {...props}
+      render={componentProps => (
+        TokenService.hasAuthToken()
+          ? <Redirect to={'/'} />
+          : <Component {...componentProps} />
+      )}
+    />
+  )
+}
